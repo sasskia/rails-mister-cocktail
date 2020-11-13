d5e14d28a01b7eaeb6eef5f5cf5133a802feb33d
Saskia Knauf <saskia.knauf@gmail.com>
2020-11-13 16:12:40 +0000

    app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Ingredient.destroy_all
+
+puts "Creating ingredients..."
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "Vodka")
+Ingredient.create(name: "Baileys")
+Ingredient.create(name: "salt")
+Ingredient.create(name: "Tequila")
+Ingredient.create(name: "Sambuca")
+Ingredient.create(name: "Gin")
+Ingredient.create(name: "tonic water")
+Ingredient.create(name: "Sprite")
+Ingredient.create(name: "fruit")
+Ingredient.create(name: "Pimm's")
